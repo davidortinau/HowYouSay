fadce0e7907c90cd7e99204d3a29d6244c864f6c
David Ortinau <david.ortinau@microsoft.com>
2017-12-27 11:02:28 -0600

    updating to nightly XF and rolling in CSS

---
+++ HowYouSay.Shared/Views/Cells/MenuCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HowYouSay.Shared.Views.Cells
+{
+	public partial class MenuCell : ViewCell
+	{
+		public MenuCell()
+		{
+			InitializeComponent();
+		}
+	}
+}
