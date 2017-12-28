820d122c0ec8a460cebd1860f3e5fd62e5563cdf
David Ortinau <david.ortinau@microsoft.com>
2017-12-28 14:37:28 -0600

    string replace gulp for caret. Styled home view, detail view, card view.

--- HowYouSay.Shared/Styles/sass/gulpfile.js
+++ HowYouSay.Shared/Styles/sass/gulpfile.js
@@ -1,10 +1,12 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var replace = require('gulp-string-replace');
 
 gulp.task('sass', function() {
     gulp.src('*.scss')
         .pipe(sass())
+        .pipe(replace(/\\\^/g, '^'))
         .pipe(gulp.dest('../'))
 });
 
